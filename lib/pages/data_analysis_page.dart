c638841b9e85a7f1583a25a8e69d1635ad0ace1c
Ziqi Yang <mr.ziqiyang@gmail.com>
2022-04-10 09:57:13 +0800

    circular indicator

--- lib/pages/data_analysis_page.dart
+++ lib/pages/data_analysis_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ski_app/common.dart' show MyColors;
 import 'package:percent_indicator/percent_indicator.dart';
@@ -91,16 +92,84 @@ class _DataAnalysisPageState extends State<DataAnalysisPage> {
 
   _circularIndicator(BuildContext context) {
     // FIXME add module
-    return CircularPercentIndicator(
+    return Container(
+        decoration: BoxDecoration(
+          boxShadow: const [BoxShadow(color: Colors.grey, blurRadius: 20.0)],
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(130),
+        ),
+      child: CircularPercentIndicator(
         startAngle: 180,
         radius: 130,
         lineWidth: 20,
         animation: true,
         animationDuration: 1000,
         percent: 0.7,
-        center: const Text("70.0%"),
+        center: _circularIndicatorCenter(context),
         circularStrokeCap: CircularStrokeCap.round,
         progressColor: Colors.blue,
+      )
+    );
+  }
+
+
+  _circularIndicatorCenter(BuildContext context){
+    TextStyle timeTextStyle = const TextStyle(fontSize: 20);
+    TextStyle pmtTextStyle = const TextStyle(color: Colors.black54, fontSize: 12,);
+    TextStyle scoreStyle = const TextStyle(color: Colors.blue, fontSize: 40, fontWeight: FontWeight.bold);
+
+    String _startTime = "00:00";
+    String _endTime = "00:00";
+    int _score = 0;
+
+    if (_singleDataModle != null) {
+      _startTime = _singleDataModle!.startTime;
+      _endTime = _singleDataModle!.endTime;
+      _score = _singleDataModle!.score;
+    }
+
+    return Container(
+        height: 110 * 2,
+        width: 110 * 2,
+        decoration: BoxDecoration(
+            color: MyColors.lightGrey,
+            borderRadius: BorderRadius.circular(130),
+        ),
+      child: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(height: 45,),
+          IntrinsicHeight(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Text(_startTime, style: timeTextStyle,),
+                    Text("开始", style: pmtTextStyle,)
+                  ],
+                ),
+                const VerticalDivider(
+                  color: Colors.grey,
+                  width: 24,
+                  thickness: 1,
+                  indent: 6,
+                  endIndent: 6,
+                ),
+                Column(
+                  children: [
+                    Text(_endTime, style: timeTextStyle,),
+                    Text("结束", style: pmtTextStyle,)
+                  ],
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 8,),
+          Text(_score.toString(),style: scoreStyle,),
+          Text("分数", style: pmtTextStyle,)
+        ],
+      ),
       );
   }
 
